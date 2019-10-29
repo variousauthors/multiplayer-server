d970564ae4c14a2c7341f8cb9e237405836d8901
various.authors <various.authors@gmail.com>
2019-10-28 19:00:42 -0700

    finished the tutorial

--- src/server.js
+++ src/server.js
@@ -18,10 +18,31 @@ server.listen(5000, function() {
   console.log('Starting server on port 5000');
 });
 
-// Add the WebSocket handlers
+var players = {};
 io.on('connection', function(socket) {
+  socket.on('new player', function() {
+    players[socket.id] = {
+      x: 300,
+      y: 300
+    };
+  });
+  socket.on('movement', function(data) {
+    var player = players[socket.id] || {};
+    if (data.left) {
+      player.x -= 5;
+    }
+    if (data.up) {
+      player.y -= 5;
+    }
+    if (data.right) {
+      player.x += 5;
+    }
+    if (data.down) {
+      player.y += 5;
+    }
+  });
 });
 
 setInterval(function() {
-  io.sockets.emit('message', 'hi!');
-}, 1000);
+  io.sockets.emit('state', players);
+}, 1000 / 60);
